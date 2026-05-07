99de127d369219f5080b8ca640449501817293ac
hechun <113486741+chun-he-316@users.noreply.github.com>
2026-05-07 19:17:07 +0800

    Add AS event calculation commands to Pipeline.sh
    Added commands to calculate alternative splicing events for multiple input files using suppa.py.

--- codes/3_Alternative splicing analysis/Pipeline.sh
+++ codes/3_Alternative splicing analysis/Pipeline.sh
@@ -1,1 +1,4 @@
-
+# Calculate AS events
+python suppa.py generateEvents -i sgd.express.gtf -o sgd.events -f ioe -e SE SS MX RI FL
+python suppa.py generateEvents -i sgr.express.gtf -o sgr.events -f ioe -e SE SS MX RI FL
+python suppa.py generateEvents -i ca.express.gtf -o ca.events -f ioe -e SE SS MX RI FL

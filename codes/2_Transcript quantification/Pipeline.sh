f1f1d56dc1bb782b422dc6673c97bacb6f47468e
hechun <113486741+chun-he-316@users.noreply.github.com>
2026-05-07 18:55:38 +0800

    Add quality control and expression calculation steps

--- codes/2_Transcript quantification/Pipeline.sh
+++ codes/2_Transcript quantification/Pipeline.sh
@@ -1,1 +1,6 @@
+# Quality Control
+fastp -i *_R1.fastq.gz -I *_R2.fastq.gz -o *_R1_clean.fastq.gz -O *_R2_clean.fastq.gz -c -h fastp.html -j fastp.json 
 
+# Calculate expression
+salmon index -t  transcript.fasta -i SalmonIndex -k 31 --keepDuplicates
+salmon quant -i SalmonIndex -l A -1 *_R1_clean.fastq.gz -2 *_R2_clean.fastq.gz -o  *.quant  --validateMappings

f60666336796b82a2535970e07a1ff2d1c7b963d
hechun <113486741+chun-he-316@users.noreply.github.com>
2026-05-07 20:21:53 +0800

    Add Bigwig file generation steps to Pipeline.sh
    Added steps to generate Bigwig files for visualization in IGV.

--- codes/2_Transcript quantification/Pipeline.sh
+++ codes/2_Transcript quantification/Pipeline.sh
@@ -4,3 +4,10 @@ fastp -i *_R1.fastq.gz -I *_R2.fastq.gz -o *_R1_clean.fastq.gz -O *_R2_clean.fas
 # Calculate expression
 salmon index -t  transcript.fasta -i SalmonIndex -k 31 --keepDuplicates
 salmon quant -i SalmonIndex -l A -1 *_R1_clean.fastq.gz -2 *_R2_clean.fastq.gz -o  *.quant  --validateMappings
+
+# Obtain Bigwig files for visualization in IGV
+# The bigWig files were generated following the workflow provided in PipeRNAseq
+# merge bigwig files
+bigWigMerge PpCAa.Ppup.sorted.bedGraph.bw PpCAb.Ppup.sorted.bedGraph.bw PpCAc.Ppup.sorted.bedGraph.bw PpCA.merge.bedGraph
+sort -k1,1 -k2,2n PpCA.merge.bedGraph > PpCA.merge.sort.bedGraph
+bedGraphToBigWig PpCA.merge.sort.bedGraph Ppup.ChromInfo.txt PpCA.merge.bw
